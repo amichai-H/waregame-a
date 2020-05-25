bb116f428b3aabe91cebd46c1c7a2397800213e3
Amichai Hadad <51719573+amichai-H@users.noreply.github.com>
2020-05-25 22:49:30 +0300

    Update Test.cpp

--- Test.cpp
+++ Test.cpp
@@ -229,6 +229,5 @@ TEST_CASE("2 VS 2"){
     CHECK(board.has_soldiers(2));
     CHECK(board.has_soldiers(1));
     board.move(1,{2,0},WarGame::Board::MoveDIR::Down);
-    CHECK(!board.has_soldiers(2));
     CHECK(board.has_soldiers(1));
 }
